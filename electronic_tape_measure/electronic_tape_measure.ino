fa3de96f1436be7ce14d27372fefe1a6a11e0e6b
liyakuruvilla <158122555+liyakuruvilla@users.noreply.github.com>
2024-11-05 12:18:05 -0500

    Update electronic_tape_measure.ino
    Liya Nov 5 morning

--- electronic_tape_measure/electronic_tape_measure.ino
+++ electronic_tape_measure/electronic_tape_measure.ino
@@ -1,7 +1,11 @@
 /*
  * MTE 201 Measurement System Project Code
  * 10/31/2024
+ * Code mostly taken from https://github.com/mo-thunderz/RotaryEncoder and https://docs.arduino.cc/learn/electronics/lcd-displays/
  */
+
+// include the library code:
+#include <LiquidCrystal.h>
 
 // macro definition for the rotatary encoder, see: https://components101.com/sites/default/files/component_datasheet/KY-04-Rotary-Encoder-Datasheet.pdf
 // TODO: set to correct values
@@ -10,6 +14,11 @@
 #define SW_PIN 4 // the built in switch
 
 // volatile unsigned int foo; <= see: https://gammon.com.au/interrupts
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 unsigned long _lastIncReadTime = micros(); 
 unsigned long _lastDecReadTime = micros(); 
@@ -23,7 +32,7 @@ void setup() {
   // encoder pin definition
   pinMode(CLK_PIN, INPUT_PULLUP);
   pinMode(DT_PIN, INPUT_PULLUP);
-  //pinMode(SW_PIN, INPUT_PULLUP);
+  pinMode(SW_PIN, INPUT_PULLUP);
   // interrupts the microcontroller and tells it to run a function on the rising edge of the CLK_PIN
   // using this so we don't miss any cycles that the rotary encoder is outputting
   // see: https://docs.arduino.cc/language-reference/en/functions/external-interrupts/attachInterrupt/
@@ -34,15 +43,31 @@ void setup() {
   // Start the serial monitor to show output
   Serial.begin(115200);
 
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  //lcd.print("hello, world!");
+
 }
 
 void loop() {
     static int lastCounter = 0;
 
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+
   // If count has changed print the new value to serial
   if(counter != lastCounter){
-    Serial.println(counter);
+    //Serial.println(counter);
+    // print the the counter
+    lcd.print(counter);
     lastCounter = counter;
+  }
+
+  // If button on encoder (SW_PIN) is pressed, reset
+  if(SW_PIN){
+    counter = 0;
   }
 }
 
